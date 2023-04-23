8aa67051868d1213ae7f0c27103ebd776dc0f208
Mauricio Garcia <maupessoagarcia@yahoo.com>
2023-04-23 11:45:30 +0200

    Random num generation + clear os

--- guessing_game.py
+++ guessing_game.py
@@ -1,2 +1,14 @@
 import random
+import os
 
+os.system("cls")
+
+print("--------------------------")
+print("Welcome to Guessing Game")
+print("--------------------------")
+print()
+
+random_number = random.randint(1,100)
+
+print(random_number)
+print()
